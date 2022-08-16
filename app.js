1c383c743c3a2d37876bd183cda6e109367382cf
cge1023 <ge931023@gmail.com>
2022-08-16 18:51:19 +0900

    media page in progress

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
-const express = require("express"); //express 모델 불러오기 
-const app = express();   //app 에 담기
+const express = require("express"); //express 모델 불러오기
+const app = express(); //app 에 담기
 app.set("view engine", "ejs"); //html 파일 불러오기
 // app.use( express.static('publuic'));  //public/img.panda.jpeg
-app.use( '/static', express.static(__dirname+'/static')); // 접근하기 위해서  /static/img/panda.jpeg 
+app.use("/static", express.static(__dirname + "/static")); // 접근하기 위해서  /static/img/panda.jpeg
 
 const port = 8080; //8000번 포트로 연결
 //ip:8000/
-app.get("/",(req,res)=>{//다음 주소를 정해주는 문자열 req(request약자)클라이언트가 사버에게    res(restuns)서버가 클라이언트한테 보내는 응답
-    res.render("main"); 
-
-}) 
+app.get("/", (req, res) => {
+  //다음 주소를 정해주는 문자열 req(request약자)클라이언트가 사버에게    res(restuns)서버가 클라이언트한테 보내는 응답
+  res.render("main");
+});
 
 //로그인 폼으로 이동
-app.get("/loginForm",(req,res)=>{
-    res.render("loginForm.ejs");
-})
+app.get("/loginForm", (req, res) => {
+  res.render("loginForm.ejs");
+});
 
 //회원가입 폼으로 이동
-app.get("/joinForm",(req,res)=>{
-    res.render("joinForm.ejs");
-})
+app.get("/joinForm", (req, res) => {
+  res.render("joinForm.ejs");
+});
 
 //자유게시판으로 이동
-app.get("/ddd",(req,res)=>{
-    res.render("ddd.ejs");
-})
+app.get("/ddd", (req, res) => {
+  res.render("ddd.ejs");
+});
 
-app.get("/main",(req,res)=>{
-    res.render("main");
-})
+app.get("/main", (req, res) => {
+  res.render("main");
+});
 
+//미디어로 이동
+app.get("/media", (req, res) => {
+  res.render("media");
+});
 
-app.listen(port,()=>{
-    console.log("server open:",port);
-})    //서버를 만드는 코드들
+app.listen(port, () => {
+  console.log("server open:", port);
+}); //서버를 만드는 코드들
---
+++ static/js/media.js
@@ -0,0 +1,20 @@
+let items = document.querySelectorAll(
+  "#featureContainer .carousel .carousel-item"
+);
+items.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
+$(document).ready(function () {
+  $("#featureCarousel").carousel({ interval: false });
+  $("#featureCarousel").carousel("pause");
+});
