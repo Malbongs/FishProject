356033e63edcbd74f34d63770ed257cd5a845305
cge1023 <ge931023@gmail.com>
2022-08-17 15:42:16 +0900

    media page completed

--- static/js/media.js
+++ static/js/media.js
@@ -1,20 +1,107 @@
-let items = document.querySelectorAll(
-  "#featureContainer .carousel .carousel-item"
-);
-items.forEach((el) => {
-  const minPerSlide = 4;
-  let next = el.nextElementSibling;
-  for (var i = 1; i < minPerSlide; i++) {
-    if (!next) {
-      // wrap carousel by using first child
-      next = items[0];
-    }
-    let cloneChild = next.cloneNode(true);
-    el.appendChild(cloneChild.children[0]);
-    next = next.nextElementSibling;
-  }
+let slider1 = tns({
+  container: ".my-slider-1",
+  slideBy: 1,
+  speed: 400,
+  nav: false,
+  controlsContainer: ".controls-1",
+  prevButton: ".previous-1",
+  nextButton: ".next-1",
+  responsive: {
+    1600: {
+      items: 4,
+      gutter: 20,
+    },
+    1024: {
+      items: 3,
+      gutter: 20,
+    },
+    748: {
+      items: 2,
+      gutter: 20,
+    },
+    480: {
+      items: 1,
+    },
+  },
 });
-$(document).ready(function () {
-  $("#featureCarousel").carousel({ interval: false });
-  $("#featureCarousel").carousel("pause");
+
+let slider2 = tns({
+  container: ".my-slider-2",
+  slideBy: 1,
+  speed: 400,
+  nav: false,
+  controlsContainer: ".controls-2",
+  prevButton: ".previous-2",
+  nextButton: ".next-2",
+  responsive: {
+    1600: {
+      items: 4,
+      gutter: 20,
+    },
+    1024: {
+      items: 3,
+      gutter: 20,
+    },
+    748: {
+      items: 2,
+      gutter: 20,
+    },
+    480: {
+      items: 1,
+    },
+  },
+});
+
+let slider3 = tns({
+  container: ".my-slider-3",
+  slideBy: 1,
+  speed: 400,
+  nav: false,
+  controlsContainer: ".controls-3",
+  prevButton: ".previous-3",
+  nextButton: ".next-3",
+  responsive: {
+    1600: {
+      items: 4,
+      gutter: 20,
+    },
+    1024: {
+      items: 3,
+      gutter: 20,
+    },
+    748: {
+      items: 2,
+      gutter: 20,
+    },
+    480: {
+      items: 1,
+    },
+  },
+});
+
+let slider4 = tns({
+  container: ".my-slider-4",
+  slideBy: 1,
+  speed: 400,
+  nav: false,
+  controlsContainer: ".controls-4",
+  prevButton: ".previous-4",
+  nextButton: ".next-4",
+  responsive: {
+    1600: {
+      items: 4,
+      gutter: 20,
+    },
+    1024: {
+      items: 3,
+      gutter: 20,
+    },
+    748: {
+      items: 2,
+      gutter: 20,
+    },
+    480: {
+      items: 1,
+    },
+  },
 });
