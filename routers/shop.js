b9c8172c1168d7b2191b9d992877b4314a056d3a
fearlessdeveloper <1dilumn0@gmail.com>
2022-08-19 17:29:04 +0900

    메인페이지 추가

--- routers/shop.js
+++ routers/shop.js
@@ -1,8 +1,7 @@
 const express= require("express");
 const router = express.Router();
-router.use((req,res,next)=>{
-    console.log(req.url);
-    next();
+router.get("/",(req,res)=>{
+    res.render('shop');
 });
 router.get("/:item",(req,res)=>{
     console.log("hello");
